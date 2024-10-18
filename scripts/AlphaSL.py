ca936b73e33d3956df4029842bd4edab8f023ce3
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2024-10-17 21:49:05 -0700

    Update AlphaSL.py

--- scripts/AlphaSL.py
+++ scripts/AlphaSL.py
@@ -1,16 +1,24 @@
 def AlphaSL():
   key_word = input("key word?")
   site_name = input("site name?")
-  
+
   while len(key_word) <= 10:
     key_word += key_word
-  
-  while len(site_name) <= len(key_word):
+
+  while len(site_name) <= 2 * (len(key_word)):
     site_name += site_name
-  
+
+  x = 2 if "o" in site_name else 1
+
+  cut_site_name = ""
+
+  while x < len(site_name):
+    cut_site_name += site_name[x]
+    x += 2
+
   password = ""
   for i in range(0, (len(key_word))):
-    password += site_name[i]
+    password += cut_site_name[i]
     password += key_word[i]
-  
+
   print(password)
